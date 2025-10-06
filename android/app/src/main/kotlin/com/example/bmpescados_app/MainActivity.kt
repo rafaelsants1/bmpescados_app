1dc3932adcc2e123d0b29bd873613ca0d36bca38
rafaelsants1 <faelalmeida19@gmail.com>
2025-10-06 17:07:47 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/com/example/bmpescados_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bmpescados_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:bmpescados_app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
